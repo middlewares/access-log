3ff97044fa679c17ca0f59683dc9d153f2e17a02
oscarotero <oom@oscarotero.com>
2017-03-30 19:03:26 +0200

    updated tests

--- tests/AccessLogTest.php
+++ tests/AccessLogTest.php
@@ -22,8 +22,15 @@ class AccessLogTest extends \PHPUnit_Framework_TestCase
             new AccessLog($logger),
         ], $request);
 
-        $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
+        $response = Dispatcher::run([
+            (new AccessLog($logger))->vhost(),
+        ], $request);
+
         rewind($logs);
-        $this->assertRegExp('#.* "GET /user/oscarotero/35 HTTP/1\.1" 200 0.*#', stream_get_contents($logs));
+
+        $string = stream_get_contents($logs);
+
+        $this->assertRegExp('#.* test\.INFO\: .* "GET /user/oscarotero/35 HTTP/1\.1" 200 0.*#', $string);
+        $this->assertRegExp('#.* test\.INFO\: domain\.com\:80 .* "GET /user/oscarotero/35 HTTP/1\.1" 200 0.*#', $string);
     }
 }
