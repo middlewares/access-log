b32ed64180d0a6f2170bd3272c5adb08e85ab2b2
oscarotero <oom@oscarotero.com>
2017-04-03 22:20:43 +0200

    test ipAttribute

--- tests/AccessLogTest.php
+++ tests/AccessLogTest.php
@@ -20,6 +20,8 @@ class AccessLogTest extends \PHPUnit_Framework_TestCase
             'REMOTE_ADDR' => '0.0.0.0'
         ], 'GET', 'http://domain.com/user/oscarotero/35');
         $request2 = Factory::createServerRequest([], 'POST', 'https://domain.com');
+        $request3 = Factory::createServerRequest([], 'PUT', 'https://domain.com')
+            ->withAttribute('client-ip', '1.1.1.1');
 
         Dispatcher::run([
             new AccessLog($logger),
@@ -32,6 +34,15 @@ class AccessLogTest extends \PHPUnit_Framework_TestCase
             }
         ], $request2);
 
+        Dispatcher::run([
+            (new AccessLog($logger))
+                ->vhost()
+                ->ipAttribute('client-ip'),
+            function () {
+                echo 'some content';
+            }
+        ], $request3);
+
         rewind($logs);
 
         $string = stream_get_contents($logs);
@@ -40,6 +51,7 @@ class AccessLogTest extends \PHPUnit_Framework_TestCase
         $expect = <<<EOT
 [date] test.INFO: 0.0.0.0 - - [date] "GET /user/oscarotero/35 HTTP/1.1" 200 - [] []
 [date] test.INFO: domain.com:443 - - - [date] "POST / HTTP/1.1" 200 12 [] []
+[date] test.INFO: domain.com:443 1.1.1.1 - - [date] "PUT / HTTP/1.1" 200 12 [] []
 EOT;
 
         $this->assertEquals($expect, $string);
