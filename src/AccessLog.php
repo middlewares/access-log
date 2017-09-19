a8d7253569c86580cca868e0383c8578789a626a
oscarotero <oom@oscarotero.com>
2017-09-19 22:05:58 +0200

    upgraded http-interop/http-middleware to 0.5

--- src/AccessLog.php
+++ src/AccessLog.php
@@ -2,8 +2,8 @@
 
 namespace Middlewares;
 
-use Interop\Http\ServerMiddleware\DelegateInterface;
-use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Interop\Http\Server\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
 use Middlewares\AccessLogFormats as Format;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -107,15 +107,15 @@ class AccessLog implements MiddlewareInterface
     /**
      * Process a server request and return a response.
      *
-     * @param ServerRequestInterface $request
-     * @param DelegateInterface      $delegate
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $handler
      *
      * @return ResponseInterface
      */
-    public function process(ServerRequestInterface $request, DelegateInterface $delegate)
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler)
     {
         $begin = microtime(true);
-        $response = $delegate->process($request);
+        $response = $handler->handle($request);
         $end = microtime(true);
 
         $message = $this->format;
