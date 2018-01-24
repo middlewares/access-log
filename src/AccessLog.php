57cd1351604dcf17bd2c10fed88dcfbd8ea7849d
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:15 +0100

    Code style fix

--- src/AccessLog.php
+++ src/AccessLog.php
@@ -3,11 +3,11 @@ declare(strict_types = 1);
 
 namespace Middlewares;
 
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Middlewares\AccessLogFormats as Format;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Log\LoggerInterface;
 
 class AccessLog implements MiddlewareInterface
